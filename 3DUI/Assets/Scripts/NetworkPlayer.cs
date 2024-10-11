6d1fbc844fc15ff6de00cf0ca3dfce405ec2b716
Patrick Leary <122827401+Patrick-Leary@users.noreply.github.com>
2024-10-11 17:43:09 -0400

    Multiplayer working (a little bit)

--- 3DUI/Assets/Scripts/NetworkPlayer.cs
+++ 3DUI/Assets/Scripts/NetworkPlayer.cs
@@ -1,46 +1,88 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 
 public class NetworkPlayer : NetworkBehaviour
 {
-
-    public Transform root;
     public Transform head;
     public Transform leftHand;
     public Transform rightHand;
 
     public Renderer[] meshToDisable;
 
+    private NetworkVariable<Vector3> netRootPosition = new NetworkVariable<Vector3>();
+    private NetworkVariable<Quaternion> netRootRotation = new NetworkVariable<Quaternion>();
+    private NetworkVariable<Vector3> netHeadLocalPosition = new NetworkVariable<Vector3>();
+    private NetworkVariable<Quaternion> netHeadLocalRotation = new NetworkVariable<Quaternion>();
+    private NetworkVariable<Vector3> netLeftHandLocalPosition = new NetworkVariable<Vector3>();
+    private NetworkVariable<Quaternion> netLeftHandLocalRotation = new NetworkVariable<Quaternion>();
+    private NetworkVariable<Vector3> netRightHandLocalPosition = new NetworkVariable<Vector3>();
+    private NetworkVariable<Quaternion> netRightHandLocalRotation = new NetworkVariable<Quaternion>();
+
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
-        if(IsOwner)
+        if (IsOwner)
         {
             foreach (var item in meshToDisable)
             {
-                item.enabled = false;
+                if (item != null) item.enabled = false;
             }
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(IsOwner)
+        if (IsOwner)
         {
-            root.position = VRRigReferences.Singleton.root.position;
-            root.rotation = VRRigReferences.Singleton.root.rotation;
-
-            head.position = VRRigReferences.Singleton.head.position;
-            head.rotation = VRRigReferences.Singleton.head.rotation;
-
-            leftHand.position = VRRigReferences.Singleton.leftHand.position;
-            leftHand.rotation = VRRigReferences.Singleton.leftHand.rotation;
-
-            rightHand.position = VRRigReferences.Singleton.rightHand.position;
-            rightHand.rotation = VRRigReferences.Singleton.rightHand.rotation;
+            UpdateLocalTransforms();
+            SyncPositionsServerRpc();
         }
+        else
+        {
+            UpdateNetworkTransforms();
+        }
+    }
+
+    void UpdateLocalTransforms()
+    {
+        if (VRRigReferences.Singleton != null)
+        {
+            transform.position = VRRigReferences.Singleton.root.position;
+            transform.rotation = VRRigReferences.Singleton.root.rotation;
+
+            head.localPosition = VRRigReferences.Singleton.head.localPosition;
+            head.localRotation = VRRigReferences.Singleton.head.localRotation;
+            leftHand.localPosition = VRRigReferences.Singleton.leftHand.localPosition;
+            leftHand.localRotation = VRRigReferences.Singleton.leftHand.localRotation;
+            rightHand.localPosition = VRRigReferences.Singleton.rightHand.localPosition;
+            rightHand.localRotation = VRRigReferences.Singleton.rightHand.localRotation;
+        }
+    }
+
+    void UpdateNetworkTransforms()
+    {
+        transform.position = netRootPosition.Value;
+        transform.rotation = netRootRotation.Value;
+
+        head.localPosition = netHeadLocalPosition.Value;
+        head.localRotation = netHeadLocalRotation.Value;
+        leftHand.localPosition = netLeftHandLocalPosition.Value;
+        leftHand.localRotation = netLeftHandLocalRotation.Value;
+        rightHand.localPosition = netRightHandLocalPosition.Value;
+        rightHand.localRotation = netRightHandLocalRotation.Value;
+    }
+
+    [ServerRpc]
+    void SyncPositionsServerRpc()
+    {
+        netRootPosition.Value = transform.position;
+        netRootRotation.Value = transform.rotation;
+
+        netHeadLocalPosition.Value = head.localPosition;
+        netHeadLocalRotation.Value = head.localRotation;
+        netLeftHandLocalPosition.Value = leftHand.localPosition;
+        netLeftHandLocalRotation.Value = leftHand.localRotation;
+        netRightHandLocalPosition.Value = rightHand.localPosition;
+        netRightHandLocalRotation.Value = rightHand.localRotation;
     }
 }
