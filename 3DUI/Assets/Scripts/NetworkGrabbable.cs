616948a2d511de3873d44684c86f4be39f97319c
Patrick Leary <122827401+Patrick-Leary@users.noreply.github.com>
2024-10-12 01:02:03 -0400

    Clients can now interact with objects with very little bugs (at least bugs are not known)

--- 3DUI/Assets/Scripts/NetworkGrabbable.cs
+++ 3DUI/Assets/Scripts/NetworkGrabbable.cs
@@ -2,89 +2,34 @@ using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class NetworkGrabbable : NetworkBehaviour
+public class NetworkedGrabbable : NetworkBehaviour
 {
     private XRGrabInteractable grabInteractable;
     private NetworkObject networkObject;
-    private Rigidbody rb;
-
-    private bool isBeingHeld = false;
-    private Transform attachTransform;
 
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
         grabInteractable = GetComponent<XRGrabInteractable>();
         networkObject = GetComponent<NetworkObject>();
-        rb = GetComponent<Rigidbody>();
 
         if (grabInteractable != null)
         {
             grabInteractable.selectEntered.AddListener(OnGrabbed);
-            grabInteractable.selectExited.AddListener(OnReleased);
         }
     }
 
     private void OnGrabbed(SelectEnterEventArgs args)
     {
-        if (IsServer) return;
-        RequestGrabServerRpc();
-        attachTransform = args.interactorObject.transform;
-        isBeingHeld = true;
-    }
-
-    private void OnReleased(SelectExitEventArgs args)
-    {
-        if (IsServer) return;
-        RequestReleaseServerRpc(rb.velocity, rb.angularVelocity);
-        isBeingHeld = false;
+        if (!IsOwner)
+        {
+            RequestOwnershipServerRpc(NetworkManager.Singleton.LocalClientId);
+        }
     }
 
     [ServerRpc(RequireOwnership = false)]
-    private void RequestGrabServerRpc(ServerRpcParams rpcParams = default)
+    private void RequestOwnershipServerRpc(ulong clientId)
     {
-        networkObject.ChangeOwnership(rpcParams.Receive.SenderClientId);
-        UpdateGrabbedStateClientRpc(true);
-    }
-
-    [ServerRpc(RequireOwnership = false)]
-    private void RequestReleaseServerRpc(Vector3 velocity, Vector3 angularVelocity, ServerRpcParams rpcParams = default)
-    {
-        networkObject.RemoveOwnership();
-        UpdateReleasedStateClientRpc(velocity, angularVelocity);
-    }
-
-    [ClientRpc]
-    private void UpdateGrabbedStateClientRpc(bool isGrabbed)
-    {
-        if (rb != null)
-        {
-            rb.isKinematic = isGrabbed;
-            rb.useGravity = !isGrabbed;
-        }
-    }
-
-    [ClientRpc]
-    private void UpdateReleasedStateClientRpc(Vector3 velocity, Vector3 angularVelocity)
-    {
-        if (rb != null)
-        {
-            rb.isKinematic = false;
-            rb.useGravity = true;
-            rb.velocity = velocity;
-            rb.angularVelocity = angularVelocity;
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        if (isBeingHeld && IsOwner)
-        {
-            Vector3 targetPosition = attachTransform.position;
-            Vector3 targetRotation = attachTransform.rotation.eulerAngles;
-
-            rb.MovePosition(targetPosition);
-            rb.MoveRotation(Quaternion.Euler(targetRotation));
-        }
+        networkObject.ChangeOwnership(clientId);
     }
 }
