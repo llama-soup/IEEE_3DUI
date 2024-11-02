b1a9a404370edde07177d963ce6a3d9a288df4f5
alli679 <alli@davidson.edu>
2024-11-01 22:57:42 -0400

    Replaced doors
    Changed the doors in the maze to have doorknobs on both ends and edited the script to have them open when clicked on

--- 3DUI/Assets/Open Ninety Degrees.cs
+++ 3DUI/Assets/Open Ninety Degrees.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Open : MonoBehaviour
+public class OpenNinetyDegrees : MonoBehaviour
 {
     private Renderer _renderer;
     bool open = false;
@@ -17,14 +17,16 @@ public class Open : MonoBehaviour
         if(open)
         {
             _renderer.transform.Rotate(Vector3.up, 90);
-            _renderer.transform.position = _renderer.transform.position + new Vector3(0.5f*GetComponent<Renderer>().bounds.size.x, 0, -5*GetComponent<Renderer>().bounds.size.z);
+            _renderer.transform.position = _renderer.transform.position + new Vector3(0.5f*GetComponent<Renderer>().bounds.size.z, 0, 5*GetComponent<Renderer>().bounds.size.x);
+            //
         }
         else
         {
             _renderer.transform.Rotate(Vector3.up, -90);
-            _renderer.transform.position = _renderer.transform.position + new Vector3(-0.5f*GetComponent<Renderer>().bounds.size.z, 0, 5*GetComponent<Renderer>().bounds.size.x);
+            _renderer.transform.position = _renderer.transform.position + new Vector3(-0.5f*GetComponent<Renderer>().bounds.size.x, 0, -5*GetComponent<Renderer>().bounds.size.z);
         }
-
+        //
         open = !open;
     }
+
 }
