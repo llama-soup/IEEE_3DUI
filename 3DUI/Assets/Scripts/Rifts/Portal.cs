fd086d2ddcc44ca576ed2a2fc07c10d87c72e33b
Patrick Leary <122827401+Patrick-Leary@users.noreply.github.com>
2024-11-24 17:16:02 -0500

    Portals now partially work in VR. The left eye seems to be messed up at the moment

--- 3DUI/Assets/Scripts/Rifts/Portal.cs
+++ 3DUI/Assets/Scripts/Rifts/Portal.cs
@@ -1,56 +1,80 @@
 using UnityEngine;
+using UnityEngine.XR;
+using Unity.XR.CoreUtils;
 
-public class Portal : MonoBehaviour
+public class VRPortal : MonoBehaviour
 {
     [Header("Portal Properties")]
-    public Portal otherPortal;
-    public Material portalMaterial;  // This should be the material on THIS portal's mesh renderer
+    public VRPortal otherPortal;
+    public Material leftEyePortalMaterial;
+    public Material rightEyePortalMaterial;
 
-    private Camera portalCamera;
-    private Camera playerCamera;
-    private RenderTexture renderTexture;
+    private Camera leftPortalCamera;
+    private Camera rightPortalCamera;
+    private Camera xrCamera;
+    private RenderTexture leftRenderTexture;
+    private RenderTexture rightRenderTexture;
+    private XROrigin xrOrigin;
 
     void Start()
     {
-        playerCamera = Camera.main;
-        portalCamera = GetComponentInChildren<Camera>();
+        // Get XR components
+        xrOrigin = FindObjectOfType<XROrigin>();
+        xrCamera = xrOrigin.Camera;
         
-        // Create and setup render texture
-        renderTexture = new RenderTexture(Screen.width, Screen.height, 24, RenderTextureFormat.DefaultHDR);
-        portalCamera.targetTexture = renderTexture;
+        // Setup left eye camera
+        leftPortalCamera = transform.Find("LeftPortalCamera").GetComponent<Camera>();
+        leftRenderTexture = new RenderTexture(Screen.width, Screen.height, 24, RenderTextureFormat.DefaultHDR);
+        leftPortalCamera.targetTexture = leftRenderTexture;
+        otherPortal.leftEyePortalMaterial.mainTexture = leftRenderTexture;
         
-        // Assign render texture to OTHER portal's material
-        otherPortal.portalMaterial.mainTexture = renderTexture;
+        // Setup right eye camera
+        rightPortalCamera = transform.Find("RightPortalCamera").GetComponent<Camera>();
+        rightRenderTexture = new RenderTexture(Screen.width, Screen.height, 24, RenderTextureFormat.DefaultHDR);
+        rightPortalCamera.targetTexture = rightRenderTexture;
+        otherPortal.rightEyePortalMaterial.mainTexture = rightRenderTexture;
         
-        // Setup portal camera
-        portalCamera.enabled = true;
-        portalCamera.fieldOfView = playerCamera.fieldOfView;
+        // Enable cameras
+        leftPortalCamera.enabled = true;
+        rightPortalCamera.enabled = true;
     }
 
     void LateUpdate()
     {
-        // Get player's offset from portal in local space
-        Vector3 playerOffsetFromPortal = transform.InverseTransformPoint(playerCamera.transform.position);
-        
-        // Flip the x and z coordinates to mirror the position correctly
-        playerOffsetFromPortal = new Vector3(-playerOffsetFromPortal.x, playerOffsetFromPortal.y, -playerOffsetFromPortal.z);
-        
-        // Transform the mirrored position to world space using the other portal
-        Vector3 newCameraPosition = otherPortal.transform.TransformPoint(playerOffsetFromPortal);
+        UpdatePortalCamera(rightPortalCamera, Camera.StereoscopicEye.Right);
+        UpdatePortalCamera(leftPortalCamera, Camera.StereoscopicEye.Left);
+    }
 
-        // Calculate rotation
-        Quaternion relativeRot = Quaternion.Inverse(transform.rotation) * playerCamera.transform.rotation;
+    private void UpdatePortalCamera(Camera portalCamera, Camera.StereoscopicEye eye)
+    {
+        // Get eye position from XR camera
+        Vector3 eyePos = xrCamera.GetStereoViewMatrix(eye).inverse.GetColumn(3);
+        
+        // Calculate portal offset while preserving eye separation
+        Vector3 offsetFromPortal = transform.InverseTransformPoint(eyePos);
+        // For left eye, preserve the negative x offset
+        if (eye == Camera.StereoscopicEye.Left)
+        {
+            offsetFromPortal = new Vector3(offsetFromPortal.x, offsetFromPortal.y, -offsetFromPortal.z);
+        }
+        else
+        {
+            offsetFromPortal = new Vector3(-offsetFromPortal.x, offsetFromPortal.y, -offsetFromPortal.z);
+        }
+        Vector3 newCameraPosition = otherPortal.transform.TransformPoint(offsetFromPortal);
+        
+        // Rest of the method remains the same
+        Quaternion eyeRot = xrCamera.transform.rotation;
+        Quaternion relativeRot = Quaternion.Inverse(transform.rotation) * eyeRot;
         Quaternion newRotation = otherPortal.transform.rotation * Quaternion.Euler(0, 180, 0) * relativeRot;
-
-        // Apply the new position and rotation
+        
         portalCamera.transform.SetPositionAndRotation(newCameraPosition, newRotation);
+        portalCamera.projectionMatrix = xrCamera.GetStereoProjectionMatrix(eye);
     }
 
     void OnDestroy()
     {
-        if (renderTexture != null)
-        {
-            renderTexture.Release();
-        }
+        if (leftRenderTexture != null) leftRenderTexture.Release();
+        if (rightRenderTexture != null) rightRenderTexture.Release();
     }
 }
--- 3DUI/Assets/Scripts/Rifts/PortalSurface.shader
+++ 3DUI/Assets/Scripts/Rifts/PortalSurface.shader
@@ -14,6 +14,8 @@ Shader "Custom/PortalSurface"
             HLSLPROGRAM
             #pragma vertex vert
             #pragma fragment frag
+            #pragma multi_compile_instancing
+            #pragma multi_compile _ UNITY_SINGLE_PASS_STEREO STEREO_INSTANCING_ON STEREO_MULTIVIEW_ON
             
             #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
 
@@ -21,12 +23,14 @@ Shader "Custom/PortalSurface"
             {
                 float4 positionOS : POSITION;
                 float2 uv : TEXCOORD0;
+                UNITY_VERTEX_INPUT_INSTANCE_ID
             };
 
             struct Varyings
             {
                 float4 positionCS : SV_POSITION;
                 float4 screenPos : TEXCOORD0;
+                UNITY_VERTEX_OUTPUT_STEREO
             };
 
             TEXTURE2D(_MainTex);
@@ -39,6 +43,9 @@ Shader "Custom/PortalSurface"
             Varyings vert(Attributes input)
             {
                 Varyings output;
+                UNITY_SETUP_INSTANCE_ID(input);
+                UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(output);
+                
                 output.positionCS = TransformObjectToHClip(input.positionOS.xyz);
                 output.screenPos = ComputeScreenPos(output.positionCS);
                 return output;
@@ -46,7 +53,7 @@ Shader "Custom/PortalSurface"
 
             float4 frag(Varyings input) : SV_Target
             {
-                // Convert to screen space UV coordinates
+                UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
                 float2 screenSpaceUV = input.screenPos.xy / input.screenPos.w;
                 return SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, screenSpaceUV);
             }
