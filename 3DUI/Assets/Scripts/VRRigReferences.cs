f4c8fcbd83728808ad4efa5586e32e92a0610cbf
llama-soup <43183037+llama-soup@users.noreply.github.com>
2024-12-07 20:33:14 -0500

    fixed hand tracking issues (unity is dumb)

--- 3DUI/Assets/Scripts/VRRigReferences.cs
+++ 3DUI/Assets/Scripts/VRRigReferences.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class VRRigReferences : MonoBehaviour
 {
     public static VRRigReferences Singleton;
+    
 
     public Transform root;
     public Transform head;
@@ -14,5 +16,29 @@ public class VRRigReferences : MonoBehaviour
     private void Awake()
     {
         Singleton = this;
+    }
+
+
+    [SerializeField] private InputActionReference positionAction;
+    [SerializeField] private InputActionReference rotationAction;
+
+    private void OnEnable()
+    {
+        positionAction.action.Enable();
+        rotationAction.action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        positionAction.action.Disable();
+        rotationAction.action.Disable();
+    }
+
+    private void Update()
+    {
+        Vector3 position = positionAction.action.ReadValue<Vector3>();
+        Quaternion rotation = rotationAction.action.ReadValue<Quaternion>();
+
+        Debug.Log($"Position: {position}, Rotation: {rotation}");
     }
 }
