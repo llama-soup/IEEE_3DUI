316419189ec904e71216efa2157d324678bf3ce7
emreguvenilir <50936821+emreguvenilir@users.noreply.github.com>
2024-12-10 09:15:48 -0500

    fixed collisions
    fixed collisions

--- 3DUI/Assets/Maze/Scripts Maze/Rotator.cs
+++ 3DUI/Assets/Maze/Scripts Maze/Rotator.cs
@@ -5,10 +5,27 @@ using UnityEngine;
 public class Rotator : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Renderer _renderer;
+    
 
+    void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+        if (_renderer == null)
+        {
+            Debug.LogError("Renderer is missing on the door object!");
+        }
+    }
     // Update is called once per frame
     void Update()
     {
         transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+            Debug.Log("Controller entered the door trigger!");
+            this.gameObject.SetActive(false);
+
     }
 }
