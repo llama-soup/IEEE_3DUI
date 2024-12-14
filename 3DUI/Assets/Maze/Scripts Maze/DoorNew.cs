91e599d857f04319b876d84cdb7f010d326a93a3
emreguvenilir <50936821+emreguvenilir@users.noreply.github.com>
2024-12-13 20:27:49 -0500

    please work again
    text

--- 3DUI/Assets/Maze/Scripts Maze/DoorNew.cs
+++ 3DUI/Assets/Maze/Scripts Maze/DoorNew.cs
@@ -8,6 +8,7 @@ public class DoorNew : MonoBehaviour
 {
     private Renderer _renderer;
     private bool open = false;
+    public NetworkPlayer netPlayer;
 
     void Start()
     {
@@ -18,10 +19,56 @@ public class DoorNew : MonoBehaviour
         }
     }
 
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //         Debug.Log("Controller entered the door trigger!");
+    //         if(other.CompareTag("PlayerTag") && this.gameObject.CompareTag("EnterMaze"))
+    //         {
+    //             Debug.Log("Entered the maze");
+
+    //             netPlayer = other;
+    //             netPlayer.InitializeMazeText();
+
+    //                 // Initialize the maze text elements
+
+
+    //             // Set initial text values
+    //             countText.text = "Count: 0";
+    //             timerText.text = "Time: 00:00";
+    //             environmentalFactsText.text = "TEST IF HERE";
+    //         }
+    //         ToggleDoor();
+    // }
+
     private void OnTriggerEnter(Collider other)
     {
-            Debug.Log("Controller entered the door trigger!");
-            ToggleDoor();
+        Debug.Log("Controller entered the door trigger!");
+
+        // Check if the colliding object has the correct tag and this door is tagged "EnterMaze"
+        if (other.CompareTag("PlayerTag") && this.gameObject.CompareTag("EnterMaze"))
+        {
+            Debug.Log("Entered the maze");
+
+            // Try to get the NetworkPlayer component from the colliding object
+            netPlayer = other.GetComponent<NetworkPlayer>();
+
+            if (netPlayer != null)
+            {
+                // Call the InitializeMazeText method on the NetworkPlayer
+                netPlayer.InitializeMazeText();
+
+                // Set initial text values (ensure these are properly set up in the NetworkPlayer script)
+                netPlayer.countText.text = "Count: 0";
+                netPlayer.timerText.text = "Time: 00:00";
+                netPlayer.environmentalFactsText.text = "TEST IF HERE";
+            }
+            else
+            {
+                Debug.LogError("The colliding object does not have a NetworkPlayer component!");
+            }
+        }
+
+        ToggleDoor();
     }
 
     public void ToggleDoor()
