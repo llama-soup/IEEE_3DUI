775d2166cee0531864374f1c0df335673eccce3b
JswimO5 <jmcdo1swim@gmail.com>
2024-12-15 20:28:47 -0500

    got rid of network variables for video

--- 3DUI/Assets/Scripts/ChatBox.cs
+++ 3DUI/Assets/Scripts/ChatBox.cs
@@ -11,21 +11,12 @@ public class ChatBox : NetworkBehaviour
     [SerializeField] private TMP_Text textbox;
     private List<string> messages;
     public string[] languages;
-    private List<NetworkVariable<string>> netMessage;
-    private NetworkVariable<string>[] netLanguage;
     void Start()
     {
         messages = new List<string>();
         languages = new string[2];
-        netMessage = new List<NetworkVariable<string>>();
-        netLanguage = new NetworkVariable<string>[2];
         messages.Add("This is the Chatbox. Player messages will appear here.");
         UpdateText();
-        for(int i = 0; i < languages.Length; i++){
-            languages[i] = "english";
-        }
-        ChangeNetLanguage();
-        ChangeNetMessage();
     }
 
     private void UpdateText(){
@@ -43,47 +34,13 @@ public class ChatBox : NetworkBehaviour
         string num = playerID.ToString();
         message = "player" +  num + ": " + message;
         messages.Add(message);
-        ChangeNetMessage();
         UpdateText();
     }
 
     public void Updatelanguage(int playerID, string language){
         languages[playerID] = language;
-        ChangeNetLanguage();
     }
     public string getText(){
         return textbox.text;
-    }
-    private void ChangeNetMessage(){
-        for(int i = 0; i < messages.Count; i++){
-            netMessage[i].Value = messages[i];
-        }
-    }
-    private void ChangeNetLanguage(){
-        for(int i = 0; i < languages.Length; i++){
-            Debug.Log(i);
-            netLanguage[i].Value = languages[i];
-        }
-    }
-    void Update(){
-        if(netMessage.Count > messages.Count){
-            messages.Add(netMessage[netMessage.Count-1].Value);
-            UpdateText();
-        }
-        else{
-            Debug.Log(netMessage[0].Value);
-            Debug.Log(messages[0]);
-            if(netMessage[0].Value != messages[0]){
-                for(int i = 0; i < netMessage.Count; i++){
-                    messages[i] = netMessage[i].Value;
-                }
-                UpdateText();                
-            }
-        }
-        if(netLanguage[0].Value != languages[0] || netLanguage[1].Value != languages[1]){
-            for(int i = 0; i < netLanguage.Length; i++){
-                languages[i] = netLanguage[i].Value;
-            }
-        }
     }
 }
